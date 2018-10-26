45240012707b30a1fc2a7abb6708004e4917ae58
dima.sladkin@gmail.com <Mandarun25>
2018-10-26 22:16:31 +0300

    Add Domain Model

---
+++ app/src/main/java/com/sladkin/weatherdemo/domain/entity/LocationModel.kt
@@ -0,0 +1,6 @@
+package com.sladkin.weatherdemo.domain.entity
+
+data class LocationModel(
+        val title: String,
+        val type: String
+)
---
+++ app/src/main/java/com/sladkin/weatherdemo/domain/entity/WeatherModel.kt
@@ -0,0 +1,15 @@
+package com.sladkin.weatherdemo.domain.entity
+
+data class WeatherModel(
+        val date: String,
+        val state: String,
+        val stateAbr: String,
+        val windSpeed: Float,
+        val compasPoint: String,
+        val minTemp: Int,
+        val maxTemp: Int,
+        val theTemp: Int,
+        val airPresure: Float,
+        val humidity: Float,
+        val visibility: Float
+)
