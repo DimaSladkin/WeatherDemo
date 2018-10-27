c47a10e6718d39ff0a0eede7b5ff2c9172b1f063
dima.sladkin@gmail.com <Mandarun25>
2018-10-27 16:06:53 +0300

    Domain model refactor

--- app/src/main/java/com/sladkin/weatherdemo/domain/entity/LocationModel.kt
+++ app/src/main/java/com/sladkin/weatherdemo/domain/entity/LocationModel.kt
@@ -2,5 +2,7 @@ package com.sladkin.weatherdemo.domain.entity
 
 data class LocationModel(
         val title: String,
-        val type: String
+        val type: String,
+        val sunrise: String,
+        val sunset: String
 )
